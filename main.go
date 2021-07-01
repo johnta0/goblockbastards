a6b83cb8067192f90f6b6570a48d30d68e478616
johnta0 <22534726+johnta0@users.noreply.github.com>
2021-07-01 15:39:14 +0900

    Use anaconda

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"os"
-	"github.com/dghubble/go-twitter/twitter"
+	"fmt"
+	"log"
+	"github.com/ChimeraCoder/anaconda"
 )
 
 func main() {
-	client := twitter.NewClient()
+	CK := os.Getenv("TW_CK")
+	CS := os.Getenv("TW_CS")
+	AT := os.Getenv("TW_AT")
+	AS := os.Getenv("TW_AS")
+	if CK == "" || CS == "" || AT == "" || AS == "" {
+		log.Fatal("Consumer key/secret and Access token/secret required")
+	}
+
+	anaconda.SetConsumerKey(CK)
+	anaconda.SetConsumerSecret(CS)
 }
+
+func getBlockIds(api *anaconda.TwitterApi) {
+	
+}
+
